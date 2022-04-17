c307860d199de4dd41278546a060ea33d6cec192
Akira Takahashi <pretty.audience.64b9de@gizmotik.com>
2022-04-17 15:56:02 +0900

    テスト階層変更

---
+++ tests/vivaldi_feed_exporter/test_main.py
@@ -0,0 +1,35 @@
+import pathlib
+from contextlib import suppress
+
+import pytest
+from vivaldi_feed_exporter.main import parse_args
+
+
+class TestParseArgs:
+    def test_profile_folder_mandatory(self):
+        """profile-folderは必須"""
+        with pytest.raises(BaseException), suppress(NotImplementedError):
+            parse_args(())
+
+    def test_profile_folder_argument(self):
+        """profile-folderは引数を取る"""
+        with pytest.raises(BaseException), suppress(NotImplementedError):
+            parse_args(("-p",))
+
+    def test_profile_folder_exists(self, tmp_path: pathlib.Path):
+        """profile-folderは存在する"""
+        with pytest.raises(BaseException), suppress(NotImplementedError):
+            parse_args(("-p", str(tmp_path / "nonexistent")))
+
+    def test_profile_folder_directory(self, tmp_path: pathlib.Path):
+        """profile-folderはディレクトリ"""
+        filepath = tmp_path / "empty"
+        with open(filepath, "wb") as _:
+            pass
+        with pytest.raises(BaseException), suppress(NotImplementedError):
+            parse_args(("-p", str(filepath)))
+
+    def test_profile_folder(self, tmp_path: pathlib.Path):
+        pathstr = str(tmp_path)
+        assert parse_args(("-p", pathstr)).profile_folder == tmp_path
+        assert parse_args(("--profile-folder", pathstr)).profile_folder == tmp_path
